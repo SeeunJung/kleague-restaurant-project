b91afd0583bf13e0de07605054c15f66c57f61c7
Seeun Jung <seeun.jung01@gmail.com>
2025-07-01 16:39:52 +0900

    Fix: API 중첩 호출 수정

--- app/users/me/page.tsx
+++ app/users/me/page.tsx
@@ -1,16 +1,72 @@
+'use client'
+
 import UserFavorites from '@/components/MyPage/UserHistory/UserFavorites'
 import DefaultProfile from '@/components/MyPage/UserProfile/DefaultProfile'
+import { useAuthStore } from '@/store/useAuthStore'
 import { Card, flexColIJCenter } from '@/styles/customStyle'
+import axiosInstance from '@/utils/axiosInstance'
+import { useEffect, useState } from 'react'
+
+interface FavoriteRestaurantProps {
+  id: number
+  name: string
+  category: string
+  address: string
+  stadiumId: number
+}
+
+interface FavoriteProps {
+  id: number
+  restaurantId: number
+  restaurant: FavoriteRestaurantProps
+}
+interface UserData {
+  id: number
+  nickname: string
+  email: string
+  phoneNumber: string
+  favoriteTeam: string
+  createdAt: string
+  updatedAt: string
+  favorites: FavoriteProps[]
+}
 
 export default function Page() {
+  const [user, setUser] = useState<UserData | null>(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const accessToken = useAuthStore((state) => state.accessToken)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axiosInstance.get<UserData>('/users/me')
+        setUser(res.data)
+      } catch (err) {
+        console.error('유저 정보 불러오기 실패', err)
+      }
+    }
+    fetchUser()
+  }, [])
+
+  if (!accessToken) {
+    ;<div>로그인이 필요한 페이지입니다.</div>
+  }
+  if (!user) return <div>유저 정보를 불러오는 중입니다.</div>
+
   return (
     <div className={flexColIJCenter('gap-[20px mt-6 mb-6 p-6')}>
       <div className="w-full max-w-4xl gap-4">
         <div className={Card()}>
-          <DefaultProfile />
+          {user && (
+            <DefaultProfile
+              user={user}
+              onEdit={() => setIsEditing(true)}
+            />
+          )}
         </div>
+
         <div className={Card()}>
-          <UserFavorites />
+          <UserFavorites favorites={user.favorites} />
         </div>
       </div>
     </div>
--- components/MyPage/UserHistory/UserFavorites.tsx
+++ components/MyPage/UserHistory/UserFavorites.tsx
@@ -1,12 +1,6 @@
-'use client'
-
 import RestaurantCard from '@/components/Card/RestaurantCard'
-import { useAuthStore } from '@/store/useAuthStore'
 import { mainTitle } from '@/styles/customStyle'
-import axiosInstance from '@/utils/axiosInstance'
 import { getStadiumNameById } from '@/utils/getStudiumById'
-import { useEffect, useState } from 'react'
-
 interface FavoriteRestaurantProps {
   id: number
   name: string
@@ -14,40 +8,18 @@ interface FavoriteRestaurantProps {
   address: string
   stadiumId: number
 }
-
 interface FavoriteProps {
   id: number
   restaurantId: number
   restaurant: FavoriteRestaurantProps
 }
-
-interface UserData {
+interface UserFavoritesProps {
   favorites: FavoriteProps[]
 }
 
-export default function UserFavorites() {
-  const [favorites, setFavorites] = useState<FavoriteProps[]>([])
-  const [loading, setLoading] = useState(true)
-  const accessToken = useAuthStore((state) => state.accessToken)
-
-  useEffect(() => {
-    const fetchFavorites = async () => {
-      try {
-        const res = await axiosInstance.get<UserData>('/users/me')
-        setFavorites(res.data.favorites)
-      } catch (err) {
-        console.error(
-          '즐겨찾기 목록을 불러오는 데 실패했습니다.',
-          err,
-        )
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchFavorites()
-  }, [accessToken, favorites])
-
-  if (loading) return <div>로딩 중...</div>
+export default function UserFavorites({
+  favorites,
+}: UserFavoritesProps) {
   if (favorites.length === 0)
     return <div>즐겨찾기한 식당이 없습니다.</div>
 
