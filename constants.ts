8db0c3e7834a0152cd425146d4f5e99b30f25f06
gaeunchoi <gaeun1884@naver.com>
2025-07-01 16:01:40 +0900

    Feat: 각 구단명 - 로고 이미지 파일 경로 연결

--- constants.ts
+++ constants.ts
@@ -1,5 +1,34 @@
 import { SignupForm } from './types/Auth'
 
+export const TEAM_LOGOS = {
+  울산: '/img/ulsan.png',
+  포항: '/img/pohang.png',
+  광주: '/img/gwangju.png',
+  전북: '/img/jeonbuk.png',
+  대구: '/img/daegu.png',
+  제주: '/img/jeju.png',
+  대전: '/img/daejeon.png',
+  수원FC: '/img/suwonfc.png',
+  강원: '/img/gangwon.png',
+  서울: '/img/seoul.png',
+  김천: '/img/gimcheon.png',
+  안양: '/img/anyang.png',
+  경남: '/img/gyeongnam.png',
+  김포: '/img/gimpo.png',
+  부산: '/img/busan.png',
+  부천: '/img/bucheon.png',
+  서울E: '/img/seoul-eland.png',
+  성남: '/img/seongnam.png',
+  수원: '/img/suwon.png',
+  안산: '/img/ansan.png',
+  인천: '/img/incheon.png',
+  전남: '/img/jeonnam.png',
+  충남아산: '/img/chungnam-asan.png',
+  충북청주: '/img/chungbuk-cheongju.png',
+  천안: '/img/cheonan.png',
+  화성: '/img/hwaseong.png',
+}
+
 export const KLEAGUE_TEAMS = [
   '강원',
   '광주',
