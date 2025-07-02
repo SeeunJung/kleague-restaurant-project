f982981163152f7e87d081417c70efe7c45dfffe
gaeunchoi <gaeun1884@naver.com>
2025-07-02 17:06:42 +0900

    Feat: 비로그인 상태에서 즐겨찾기 토글 누를 시 모달 이용해 로그인으로 유도

--- components/Card/FavoriteButton.tsx
+++ components/Card/FavoriteButton.tsx
@@ -1,66 +1,90 @@
 'use client'
 
-import { useAuthStore } from "@/store/useAuthStore"
-import axiosInstance from "@/utils/axiosInstance";
-import { Heart } from "lucide-react";
-import { useEffect, useState } from "react";
+import useModal from '@/hooks/useModal'
+import { useAuthStore } from '@/store/useAuthStore'
+import axiosInstance from '@/utils/axiosInstance'
+import { Heart } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import Modal from '../common/Modal'
 
-interface FavoriteButtonProps{
+interface FavoriteButtonProps {
   restaurantId: number
 }
 
-export default function FavoriteButton({ restaurantId }: FavoriteButtonProps){
-  const [isFavorite, setIsFavorite] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const accessToken = useAuthStore(state => state.accessToken);
+export default function FavoriteButton({
+  restaurantId,
+}: FavoriteButtonProps) {
+  const [isFavorite, setIsFavorite] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const accessToken = useAuthStore((state) => state.accessToken)
+  const { modalOpen, setModalOpen, modalContent, openModal } =
+    useModal()
 
   useEffect(() => {
-    const checkFavorite = async() => {
-      if(!accessToken){
-        setIsFavorite(false);
-        return;
+    const checkFavorite = async () => {
+      if (!accessToken) {
+        setIsFavorite(false)
+        return
       }
-      try{
-        const res = await axiosInstance.post('/favorites/me');
-        const favorites = res.data as {restaurantId: number}[];
-        const exists = favorites.some((fav) => fav.restaurantId === restaurantId);
+      try {
+        const res = await axiosInstance.post('/favorites/me')
+        const favorites = res.data as { restaurantId: number }[]
+        const exists = favorites.some(
+          (fav) => fav.restaurantId === restaurantId,
+        )
         setIsFavorite(exists)
-      } catch(err){
-        console.error('즐겨찾기 불러오기 실패', err);
+      } catch (err) {
+        console.error('즐겨찾기 불러오기 실패', err)
       }
     }
     checkFavorite()
   }, [restaurantId, accessToken])
 
-  const toggleFavorite = async() => {
-    if(!accessToken){
-      alert('즐겨찾기는 로그인 후에 사용 가능합니다.')
-      return;
+  const toggleFavorite = async () => {
+    if (!accessToken) {
+      openModal({
+        isError: false,
+        title: '로그인이 필요합니다',
+        description: '즐겨찾기 기능을 사용하려면 로그인 해주세요.',
+        onBtnClick: () => {
+          window.location.href = '/login'
+        },
+      })
+      return
     }
-    if(isLoading) return;
-    setIsLoading(true);
+    if (isLoading) return
+    setIsLoading(true)
 
-    try{
-      if(isFavorite){
-        await axiosInstance.delete(`/favorites/${restaurantId}`);
-        setIsFavorite(false);
-      } else{
-        await axiosInstance.post(`/favorites/${restaurantId}`);
-        setIsFavorite(true);
+    try {
+      if (isFavorite) {
+        await axiosInstance.delete(`/favorites/${restaurantId}`)
+        setIsFavorite(false)
+      } else {
+        await axiosInstance.post(`/favorites/${restaurantId}`)
+        setIsFavorite(true)
       }
-    } catch(error){
+    } catch (error) {
       console.error('즐겨찾기 토글 실패', error)
-    } finally{
-      setIsLoading(false);
+    } finally {
+      setIsLoading(false)
     }
   }
 
-  return(
-    <button className={`p-1 rounded-full`}
+  return (
+    <button
+      className={`p-1 rounded-full`}
       onClick={toggleFavorite}
       disabled={isLoading}
     >
-      <Heart fill={isFavorite ? 'red' : 'none'} color={isFavorite ? 'red' : 'black'} />
+      <Heart
+        fill={isFavorite ? 'red' : 'none'}
+        color={isFavorite ? 'red' : 'black'}
+      />
+      <Modal
+        isOpen={modalOpen}
+        onOpenChange={setModalOpen}
+        contents={modalContent}
+      />
     </button>
   )
 }
