5ccdf421ca2c660fb8b4fdd089b182703ee6927d
gaeunchoi <gaeun1884@naver.com>
2025-07-02 16:32:13 +0900

    Feat: 리뷰 관련 API 완성

--- services/review.ts
+++ services/review.ts
@@ -1,10 +1,22 @@
-import { Review } from '@/types/Restaurant'
+import { Review } from '@/types/Review'
 import axiosInstance from '@/utils/axiosInstance'
 
 export const createReview = async (
   payload: Pick<Review, 'restaurantId' | 'rating' | 'content'>,
 ) => {
   const res = await axiosInstance.post('/reviews', payload)
+  return res.data
+}
 
+export const editReview = async (
+  id: number,
+  payload: Partial<Pick<Review, 'rating' | 'content'>>,
+) => {
+  const res = await axiosInstance.patch(`/reviews/${id}`, payload)
+  return res.data
+}
+
+export const deleteReview = async (payload: Pick<Review, 'id'>) => {
+  const res = await axiosInstance.delete(`reviews/${payload.id}`)
   return res.data
 }
