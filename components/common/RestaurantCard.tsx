d4849b54c083d79628f8f3bb5a36a1e309813bb4
gaeunchoi <gaeun1884@naver.com>
2025-07-03 13:24:09 +0900

    Feat: 맛집 카드 컴포넌트 카드형 / 리스트형 기능 추가

--- components/common/RestaurantCard.tsx
+++ components/common/RestaurantCard.tsx
@@ -4,26 +4,38 @@ import {
   flexCol,
   flexColIJCenter,
   flexRow,
+  flexRowICenter,
   subTitle,
 } from '@/styles/customStyle'
-import { Restaurant } from '@/types/Restaurant'
+import { Restaurant, RestaurantViewType } from '@/types/Restaurant'
 import { cn } from '@/utils/cn'
 import RestaurantReviewStar from './RestaurantReviewStar'
 import { useRouter } from 'next/navigation'
 import FavoriteButton from '../Card/FavoriteButton'
+import { memo } from 'react'
 
 type RestaurantCardProps = {
   restaurant: Omit<Restaurant, 'reviews'>
+  viewMode: RestaurantViewType
 }
 
-function RestaurantCard({ restaurant }: RestaurantCardProps) {
+function RestaurantCard({
+  restaurant,
+  viewMode,
+}: RestaurantCardProps) {
   const router = useRouter()
+
   const handleClick = (id: number) => {
     router.push(`/restaurants/${id}`)
   }
 
   return (
-    <div className={Card('p-0', 'overflow-hidden')}>
+    <div
+      className={Card('p-0', 'overflow-hidden', {
+        'flex flex-col': viewMode === '카드형',
+        'flex flex-row items-center': viewMode === '리스트형',
+      })}
+    >
       <div
         className={flexColIJCenter(
           'w-full',
@@ -31,6 +43,10 @@ function RestaurantCard({ restaurant }: RestaurantCardProps) {
           'bg-gray-300',
           'text-gray-600',
           'relative',
+          {
+            'w-full h-35': viewMode === '카드형',
+            'w-40 h-full': viewMode === '리스트형',
+          },
         )}
       >
         이미지 준비중
@@ -77,7 +93,7 @@ function RestaurantCard({ restaurant }: RestaurantCardProps) {
           avgRating={restaurant.avgRating}
           reviewCount={restaurant.reviewCount}
         />
-        <div className={flexRow('items-start', 'justify-between')}>
+        <div className={flexRowICenter('justify-between')}>
           <div>{restaurant.stadium.name}</div>
           <div
             className={button()}
@@ -91,4 +107,4 @@ function RestaurantCard({ restaurant }: RestaurantCardProps) {
   )
 }
 
-export default RestaurantCard
+export default memo(RestaurantCard)
