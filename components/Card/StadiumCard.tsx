703cc06aa4c1fc839a6fbc2769fe6f969a8a91fa
Seeun Jung <seeun.jung01@gmail.com>
2025-07-10 16:41:40 +0900

    Style: 구장 목록 페이지 UI 변경

--- components/Card/StadiumCard.tsx
+++ components/Card/StadiumCard.tsx
@@ -1,10 +1,16 @@
 'use client'
 
-import { Card, flexRow } from '@/styles/customStyle'
+import {
+  button,
+  Card,
+  flexCol,
+  flexRowICenter,
+  mainTitle,
+  subTitle,
+} from '@/styles/customStyle'
 import { Stadium } from '@/types/Stadium'
 import { useRouter } from 'next/navigation'
-import { cn } from '@/utils/cn'
-import StadiumInfo from './../Stadium/StadiumInfo'
+import Image from 'next/image'
 
 interface StadiumCardProps {
   stadium: Stadium
@@ -19,19 +25,31 @@ export default function StadiumCard({ stadium }: StadiumCardProps) {
 
   return (
     <div
-      className={cn(
-        flexRow(Card()),
-        'gap-2 transition cursor-pointer',
-      )}
+      className={flexCol(Card(), 'gap-5 transition cursor-pointer')}
       onClick={handleClick}
     >
-      <StadiumInfo
-        key={stadium.id}
-        name={stadium.name}
-        team={stadium.team}
-        address={stadium.address}
-        logo={stadium.logo}
-      />
+      <div className={flexRowICenter('justify-between')}>
+        <Image
+          src={stadium.logo}
+          alt={`${stadium.team} 로고`}
+          width={40}
+          height={40}
+          className="w-auto"
+        />
+        <span className="inline-block w-fit mt-1 px-2 py-1 text-[10px] font-semibold bg-gray-300 bg-opacity-75 rounded-full">
+          {stadium.team}
+        </span>
+      </div>
+      <div className={flexCol()}>
+        <div className={mainTitle()}>{stadium.name}</div>
+        <div className={subTitle()}>{stadium.address}</div>
+        <button
+          className={button('rounded-lg p-2 text-sm')}
+          onClick={handleClick}
+        >
+          상세보기
+        </button>
+      </div>
     </div>
   )
 }
